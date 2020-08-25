8ed550b600000cb9caf05a3cc901a13b34f5adef
sin00528 <sin00528@gmail.com>
2020-08-25 10:53:00 +0900

    update ch.07

---
+++ 07.Legal_Communicable_Disease/02.HAV_Gwangju.py
@@ -0,0 +1,84 @@
+#%%
+import pandas as pd
+import numpy as np
+
+from sklearn.metrics import mean_squared_error
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.linear_model import Lasso, Ridge, LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.neural_network import MLPRegressor
+
+from xgboost import XGBRegressor
+from catboost import CatBoostRegressor
+
+import matplotlib.pyplot as plt
+
+#%%
+data = pd.read_excel("./data/HAV_Gwangju.xlsx", encoding="utf-8")
+
+#%%
+X = pd.get_dummies(data.iloc[:, 2:])
+y = data["환자"]
+
+#%%
+X_train = X.iloc[:-5, :]
+X_test = X.iloc[-5:, :]
+y_train = y[:-5]
+y_test = y[-5:]
+
+#%%
+model_list = [RandomForestRegressor,
+              GradientBoostingRegressor,
+              Lasso,
+              Ridge,
+              LinearRegression,
+              KNeighborsRegressor,
+              MLPRegressor,
+              XGBRegressor,
+              CatBoostRegressor]
+
+score_list = []
+
+
+#%%
+def auto_learning(m_name, X, y):
+    print(m_name.__name__)
+    model = m_name().fit(X, y)
+    pred = model.predict(X)
+    loss = mean_squared_error(y, pred, squared=False)
+    print("RMSE: ", loss)
+    score_list.append([str(m_name.__name__), round(loss, 2)])
+
+"""
+def preprocessingNA(m_name, X, y, test):
+    predict_list = []
+    model = m_name().fit(X, y)
+    y_pred = model.predict(test)
+    predict_list.append(y_pred)
+"""
+
+#%%
+for m_name in model_list:
+    auto_learning(m_name, X_train, y_train)
+
+#%%
+# show models' loss Value
+score_df = pd.DataFrame(score_list, columns=["Model", "RMSE"])
+print(score_df)
+
+#%%
+# Draw a graph of Catboost
+model = CatBoostRegressor().fit(X_train, y_train, verbose=False)
+pred = model.predict(X_test)
+
+num = np.linspace(0, 10, 10)
+plt.scatter(y_test, pred)
+plt.plot(num)
+plt.xlim(-0.1, 1)
+plt.ylim(-0.1, 1)
+plt.ylabel('Ground Truth')
+plt.xlabel('Prediction')
+plt.savefig('HAV_Gwangju.png')
+plt.show()
+
+
